3456b461f719500663c2a78d524e926274a86a10
evansnguyen <long.np68@gmail.com>
2024-09-23 00:43:34 +0800

    feat: add END_DATE to CountDownTimer

--- src/components/CountDownTimer/CountDownTimer.jsx
+++ src/components/CountDownTimer/CountDownTimer.jsx
@@ -3,18 +3,47 @@ import React, { useEffect, useRef, useState } from "react";
 import CountDownCard from "./CountDownCard.jsx";
 import "./index.css";
 
+const END_DATE = "2024-10-11T09:00:00.000Z";
 const CountDownTimer = () => {
+  function timeUntil(targetDate) {
+    const now = new Date(); // Get the current date and time
+    const target = new Date(targetDate); // Set the target date
+
+    // Calculate the difference in milliseconds
+    const timeDiff = target.getTime() - now.getTime();
+
+    // If the date has passed, return 0 for all units
+    if (timeDiff <= 0) return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+
+    // Calculate days, hours, minutes, and seconds
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  }
+
   //card ref
   const SecondsCardRef = useRef(null);
   const MinutesCardRef = useRef(null);
   const HoursCardRef = useRef(null);
   const DaysCardRef = useRef(null);
-  //state
-  const [days, setDays] = useState(14);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
 
+  const {
+    days: daysUntil,
+    hours: hoursUntil,
+    minutes: minutesUntil,
+    seconds: secondsUntil,
+  } = timeUntil(END_DATE);
+
+  //state
+  const [days, setDays] = useState(daysUntil);
+  const [hours, setHours] = useState(hoursUntil);
+  const [minutes, setMinutes] = useState(minutesUntil);
+  const [seconds, setSeconds] = useState(secondsUntil);
   useEffect(() => {
     seconds === 0 && setSeconds(59);
     minutes === 0 && setMinutes(59);
@@ -37,7 +66,6 @@ const CountDownTimer = () => {
     }
   }, [minutes, hours]);
   useEffect(() => {
-    days === 14 && setDays(13);
     hours === 0 &&
       setDays(days - 1) &&
       DaysCardRef.current.classList.toggle("rotate");
